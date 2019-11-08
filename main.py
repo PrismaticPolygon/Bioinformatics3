2f4e77d4d60807e0b32810f7d87a081b2c6b1dc9
PrismaticPolygon <domwhite97@gmail.com>
2019-11-08 14:58:20 +0000

    Part 1 complete

--- main.py
+++ main.py
@@ -10,148 +10,127 @@ import numpy as np
 # s[1..j] and t[1...i]
 # For global alignment... the best score is the last value in the table.
 
-def c(c1, c2, alphabet, scoring_matrix):
+def c(c1, c2):
 
-    i = alphabet.index(c1)
-    j = alphabet.index(c2)
+    global ALPHABET, SCORING_MATRIX
 
-    return scoring_matrix[i, j]
+    i = ALPHABET.index(c1)
+    j = ALPHABET.index(c2)
 
-
-def dynprog(alphabet, scoring_matrix, seq1, seq2):
-
-    alphabet += "_"
-    scoring_matrix = np.array(scoring_matrix)
-
-    backtrack_scoring_matrix = build_backtrack_scoring_matrix(seq1, seq2, alphabet, scoring_matrix)
-    backtrack_matrix = build_backtrack_matrix(len(seq1), len(seq2))
-
-    print(scoring_matrix)
-    print(backtrack_scoring_matrix)
-    print(backtrack_matrix)
-
-    print(scoring_matrix.shape)
-    print(backtrack_scoring_matrix.shape)
-    print(backtrack_matrix.shape)
-
-    # So my backtrack matric is now too small.
-    # Ah. It depends on the sequence character. Logically, then, we pass it
-    # through and calculate it that way. Let's try that.
-    # Where am I getting this -2 from?
-    # Ah. The values of D[i, j] must be explicitly stated. When there are no matrix elements above and left.
-    # This corresponds to aligning strings with gaps....
-
-    # Okay. Now we're in business. I can't imagine that this still works, mind.
-
-    # for i in range(1, len(seq1) + 1):
-    #
-    #     for j in range(1, len(seq2) + 1):
-    #
-    #         score = max_value(
-    #             c(seq1[i - 1], seq2[j - 1], alphabet, scoring_matrix) + backtrack_scoring_matrix[i - 1][j - 1],
-    #             backtrack_scoring_matrix[i - 1][j] - 2, backtrack_scoring_matrix[i][j - 1] - 2,
-    #             backtrack_matrix, i, j
-    #         )
-    #
-    #         backtrack_scoring_matrix[i, j] = score
-    #
-    # best_score = backtrack_scoring_matrix[-1][-1]
-    # #
-    # print(backtrack_matrix)
-    # print(backtrack_scoring_matrix)
-    # print(best_score)
-    #
-    # seq1, seq2 = track_back(backtrack_matrix, seq1, seq2)
-    #
-    # print(seq1, seq2)
-    # # print(best_score)
-    #
-    return 0, 1, 2
+    return SCORING_MATRIX[i, j]
 
 
+def dynprog(alphabet, scoring_matrix, s, t):
 
-def track_back(backtrack_matrix, seq1, seq2):
+    global ALPHABET, SCORING_MATRIX
 
-    # Okay. Now things ARE getting interesting.
+    ALPHABET = alphabet + "_"
+    SCORING_MATRIX = np.array(scoring_matrix)
 
-    j = len(seq1)
-    i = len(seq2)
+    backtrack_scoring_matrix = build_backtrack_scoring_matrix(s, t)
+    backtrack_matrix = build_backtrack_matrix(len(s), len(t))
 
-    fin1 = ''
-    fin2 = ''
+    for i in range(1, len(s) + 1):
 
-    # Ah. Basically, a back matrix.
+        for j in range(1, len(t) + 1):
+
+            match = backtrack_scoring_matrix[i - 1][j - 1] + c(s[i - 1], t[j - 1])
+            gap_s = backtrack_scoring_matrix[i][j - 1] + c(s[i - 1], "_")
+            gap_t = backtrack_scoring_matrix[i - 1][j] + c(t[j - 1], "_")
+
+            direction, score = max_value(match, gap_s, gap_t)
+
+            backtrack_matrix[i, j] = direction
+            backtrack_scoring_matrix[i, j] = score
+
+
+    best_score = backtrack_scoring_matrix[-1][-1]
+    seq1, seq2 = track_back(backtrack_matrix, s, t)
+
+    matches = []
+    mismatches = []
+
+    for i in range(len(seq1)):
+
+        if seq1[i] == seq2[i]:
+
+            matches.append(i)
+
+        else:
+
+            mismatches.append(i)
+
+    print(seq1, seq2)
+
+    return best_score, matches, mismatches
+
+
+def track_back(backtrack_matrix, s, t):
+
+    i, j = len(s), len(t)
+    seq1, seq2 = "", ""
 
     current = backtrack_matrix[-1][-1]
 
-    while current != "E":
+    while current != b'E':
 
-        if current == "D":
-
-            i -= 1
-            j -= 1
-            fin1 = seq1[j] + fin1
-            fin2 = seq2[i] + fin2
-
-        elif current == "L":
+        if current == b'D':
 
             j -= 1
-            fin1 = seq1[j] + fin1
-            fin2 = '-' + fin2
+            i -= 1
+            seq1 = s[i] + seq1
+            seq2 = t[j] + seq2
 
-        elif current == "U":
+        elif current == b'L':
 
             i -= 1
-            fin1 = '-' + fin1
-            fin2 = seq2[i] + fin2
+            seq1 = s[i] + seq1
+            seq2 = '-' + seq2
 
-        current = backtrack_matrix[i][j]
+        elif current == b'U':
 
-    return fin1, fin2
+            j -= 1
+            seq1 = '-' + seq1
+            seq2 = t[j] + seq2
+
+        current = backtrack_matrix[j][i]
+
+    return seq1, seq2
 
 
-def max_value(score_d, score_u, score_l, backtrack_matrix, i, j):
+def max_value(score_d, score_u, score_l):
 
     if score_d >= score_u and score_d >= score_l:
 
-        backtrack_matrix[i, j] = "D"
-
-        return score_d
+        return "D", score_d
 
     elif score_u >= score_l:
 
-        backtrack_matrix[i, j] = "U"
+        return "U", score_u
 
-        return score_u
-
-    backtrack_matrix[i, j] = "L"
-
-    return score_l
+    return "L", score_l
 
 
-def build_backtrack_scoring_matrix(seq1, seq2, alphabet, scoring_matrix):
+def build_backtrack_scoring_matrix(s, t):
 
-    i = len(seq1) + 1
-    j = len(seq2) + 1
-    gap_cost = c("_", "_", alphabet, scoring_matrix)
-    backtrack_scoring_matrix = np.zeros((i, j), dtype=np.int8)
+    i = len(s) + 1
+    j = len(t) + 1
+    backtrack_scoring_matrix = np.zeros((i, j), dtype="int8")
 
-    for k in range(1, i):
+    for k in range(1, max(i, j)):
 
-        backtrack_scoring_matrix[0, k] = backtrack_scoring_matrix[0, k - 1] + c(seq1[k - 1], "_", alphabet, scoring_matrix)
+        if k < j:
 
-    for k in range(1, j):
+            backtrack_scoring_matrix[0, k] = backtrack_scoring_matrix[0, k - 1] + c(t[k - 1], "_")
 
-        backtrack_scoring_matrix[k, 0] = backtrack_scoring_matrix[k - 1, 0] + c(seq2[k - 1], "_", alphabet, scoring_matrix)
+        if k < i:
 
-    backtrack_scoring_matrix[-1, 0] = backtrack_scoring_matrix[-2, 0] + gap_cost
-    backtrack_scoring_matrix[0, -1] = backtrack_scoring_matrix[0, -2] + gap_cost
+            backtrack_scoring_matrix[k, 0] = backtrack_scoring_matrix[k - 1, 0] + c(s[k - 1], "_")
 
     return backtrack_scoring_matrix
 
 
 def build_backtrack_matrix(i, j):
-    # Vectorised string operations in https://docs.scipy.org/doc/numpy/reference/routines.char.html#module-numpy.char
 
     backtrack_matrix = np.full((i + 1, j + 1), b" ", dtype="string_")
 
@@ -160,18 +139,3 @@ def build_backtrack_matrix(i, j):
     backtrack_matrix[0, 0] = "E"
 
     return backtrack_matrix
-
-
-# seq1 = "ABCACA"
-# seq2 = "BAACBC"
-# backtrack_matrix = [[]]
-# backtrack_scoring_matrix = build_backtrack_scoring_matrix(len(seq1), len(seq2))
-# backtrack_matrix = build_backtrack_matrix(len(seq1), len(seq2))
-
-
-# # So we call populate_matrices and it goes from there. That effectively IS our dynprog
-#
-# best_alignment = []
-# best1, best2, best_score = populate_matrices(seq1, seq2, scoring_matrix, backtrack_matrix)
-# best_alignment.append(best1)
-# best_alignment.append(best2)
--- test.py
+++ test.py
@@ -6,5 +6,6 @@ from main import dynprog
 
 
 a = dynprog("ABC", [[1,-1,-2,-1],[-1,2,-4,-1],[-2,-4,3,-2],[-1,-1,-2,0]], "AABBAACA", "CBACCCBA")
+# a = dynprog("ABC", [[1,-1,-2,-1],[-1,2,-4,-1],[-2,-4,3,-2],[-1,-1,-2,0]], "ABCACA", "BAACB")
 print("Score:   ", a[0])
 print("Indices: ", a[1],a[2])

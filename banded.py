6e78a34c97890a23b2f7086d8d63b577e827bdee
PrismaticPolygon <domwhite97@gmail.com>
2019-11-13 16:38:53 +0000

    Tidied up FASTA

--- banded.py
+++ banded.py
@@ -5,36 +5,167 @@ import numpy as np
 # Real-life matches often contain long strings with gap-less matches.
 # Heuristics try to find significant gap-less matches and extend them.
 
+
+# for large ktup, only entries for tuples actually found in the database should be kept, so that the index table
+# size is at most linear. In this case hashing is needed (why?)
+
+# The index table is prepared for each database sequence ahead of users' matching requests, at compilation time
+# So matching time is O(|T| x max (row_length))
+
+# Then we expand to banded DP. But how? I have a list of matches. Each has an i, j position dictating where the match
+# starts.
+
+def align(s, t, matches, k):
+
+    start = min(matches, key=lambda m: min(m[0], m[1]))
+    end = max(matches, key=lambda m: max(m[0], m[1]))
+
+    s_align = ""
+    t_align = ""
+    score = 0
+
+    print(s)
+    print(t)
+    print(matches)
+
+    match = matches[0]
+    #
+    # Out of bounds? Because I'm supposed to match with a 0?
+    # This should be dynamic programming for sure: I just can't figure out how.
+    # Actually, is it that hard?
+    for i in range(match[2]):
+
+        s_i = s[i + match[0]]
+        t_i = t[i + match[1]]
+
+        score += cost(s_i, t_i)
+
+        s_align += s_i
+        t_align += t_i
+    #
+    #
+    # print(start, end)
+    # print(s_align, t_align, score)
+
+    return 0, 0
+
+
+def build_score_matrix(s, t, sublinear=False):
+
+    size_y = len(s) + 1  # y-axis
+    size_x = len(t) + 1  # x-axis
+    shape = (2, size_x) if sublinear else (size_y, size_x)
+    D = np.zeros(shape, dtype="int8")
+
+    for y in range(1, size_y):  # y
+
+        D_y = 1 if sublinear else y
+
+        for x in range(1, size_x):
+
+            D[D_y, x] = max(
+                0,  # For local alignment
+                match(D, D_y, x, s, y, t),  # The cost of matching two characters
+                insert_gap_into_s(D, D_y, x, t),  # The cost of matching a gap in s with a character in t
+                insert_gap_into_t(D, D_y, x, s, y)  # The cost of matching a gap in t with a character in s
+            )
+
+        if sublinear and y < size_y - 1: # Copy the 1st row onto the second row unless it's the final iteration
+
+            D[0] = D[1].copy()
+            D[1] = 0
+
+    return D[1] if sublinear else D
+
+
 def banded(s, t, matches):
 
-    k = 10
+    diagonals = dict()
+    k = 9
+
+    # I'm not sure how. Actually, that's untrue. It'll be the nearest (under) multiple of k.
+    # So then I guess part of the heuristic is identifying the diagonals! I'll do the most simple kind of banded for now:
+
+    for match in matches:
+
+        x, y, length = match
+
+        i = k * ((y - x) // k)
+
+        if i not in diagonals:
+
+            diagonals[i] = [match]
+
+        else:
+
+            diagonals[i].append(match)
+
+    for key, value in diagonals.items():
+
+        s_align, t_align = align(s, t, value, k)
+
+        break
+
+
+    # And now I do gapped alignment of them. It doesn't actually have to use dynprog either.
+    # I could just use all of them.
+    # Again: it has to be sub-quadratic, and results have to be decent.
+
+    # So it's the y-offset.
+    # Yes. (0, 29, 10), (4, 36, 6) and (7, 36, 3) all lie in the same
+    # bad, which lies between (0, 27) and (0, 36).
+    # So k is like the offset now.
+    # But then: what does a value of 27 mean?
+    # It has to be taken from... not even (0, 0), actually.
+    # It could start anywhere.
+    # It's irrelevant, actually.
+    # They're essentially gradients. There we go. THAT's how to imagine it.
+
+    # Well, I don't have to for now.
+
+
+
+    # And then we sort by score.
+
+    # Long strings with gapless sections. But I've got the k wrong here.
+    # If it's out-of-bounds, count it as a 0, local-alignemtn style. That's what we're doing, after all.
+
+    # k = 9
+
+    # Heuristically find potential diagonals and evaluate them using Banded DP.
+    # So we should actually group the matches.
+    # We can again create buckets.
 
     # print(matches)
 
-    matches.sort(key=lambda x: x[1] - x[0])
-
-    matches = list(filter(lambda x: (x[1] - x[0] < k), matches))
+    # matches.sort(key=lambda x: x[1] - x[0])
+    #
+    # matches = list(filter(lambda x: (x[1] - x[0] < k), matches))
 
     # Get the top-left corner.
 
-    top_left = matches[0][0]
-    bottom_right = k + 4    # Should really be worth length
+    # We can't use dynprog. It has to be a modified version of our current scoring_matrix
+    # whereby we check for out of bounds.
+    # And so k has to be an odd number.
 
-    s = s[top_left: bottom_right]
-    t = t[top_left: bottom_right]
-
-    # But then it's still only local. NOT global. Pe
-
-    score, s_align, t_align, s_matches, t_matches = dynprog("ABC", scoring_matrix, s, t)
-
-    s_align = [align + top_left for align in s_align]
-    t_align = [align + top_left for align in t_align]
-
-    print(score)
-    print(s_align)
-    print(t_align)
-    print(s_matches)
-    print(t_matches)
+    # top_left = matches[0][0]
+    # bottom_right = k + 4    # Should really be worth length
+    #
+    # s = s[top_left: bottom_right]
+    # t = t[top_left: bottom_right]
+    #
+    # # But then it's still only local. NOT global. Pe
+    #
+    # score, s_align, t_align, s_matches, t_matches = dynprog("ABC", scoring_matrix, s, t)
+    #
+    # s_align = [align + top_left for align in s_align]
+    # t_align = [align + top_left for align in t_align]
+    #
+    # print(score)
+    # print(s_align)
+    # print(t_align)
+    # print(s_matches)
+    # print(t_matches)
 
     # This seems pretty good to me at the moment.
 
@@ -69,9 +200,73 @@ def banded(s, t, matches):
     # Such a heurst
 
 
-def cost(c1, c2):
+def score(s, t, match):
 
-    global alphabet, scoring_matrix
+    x, y, length = match
+
+    score = 0
+
+    for i in range(length):
+
+        score += cost(s[y + i], t[x + i])
+
+    return score
+
+def extend(s, t, match):
+
+    x, y, length = match
+
+    while x > 0 and y > 0:
+
+        if cost(t[x - 1], s[y - 1]) > 0:
+
+            x -= 1
+            y -= 1
+            length += 1
+
+        else:
+
+            break
+
+    while x + length + 1 < len(t) and y + length + 1 < len(s):
+
+        if cost(t[x + 1], s[y + 1]) > 0:
+
+            length += 1
+
+        else:
+
+            break
+
+    return x, y, length
+
+def fasta(s, t, ktup):
+
+    # s (database), |s| >> |t| is the y-axis
+    # t (query), is the x-axis
+
+    lookup = {"".join(i): [] for i in product(alphabet, repeat=ktup)}
+
+    for y in range(len(s) - ktup + 1):
+
+        word = s[y:y + ktup]
+        lookup[word].append(y)
+
+    matches = []
+
+    for x in range(len(t) - ktup + 1):
+
+        word = t[x:x + ktup]
+
+        for y in lookup[word]:
+
+            matches.append((x, y, ktup))
+
+    extended = [extend(s, t, match) for match in matches]
+
+    banded(s, t, extended)
+
+def cost(c1, c2):
 
     return scoring_matrix[alphabet.index(c1), alphabet.index(c2)]
 
@@ -83,113 +278,6 @@ scoring_matrix = np.array([
     [-2, -4, 3, -2],
     [-1, -1, -2, 0]
 ])
-
-#
-# Max
-# size
-# of
-# lookup is | alphabet | ^ ktup.For
-# small
-# ktup, the
-# entire
-# table is stored;
-
-
-# for large ktup, only entries for tuples actually found in the database should be kept, so that the index table
-# size is at most linear. In this case hashing is needed (why?)
-
-# The index table is prepared for each database sequence ahead of users' matching requests, at compilation time
-# So matching time is O(|T| x max (row_length))
-
-# Then we expand to banded DP. But how? I have a list of matches. Each has an i, j position dictating where the match
-# starts.
-
-def score_match(s, t, match):
-
-    x_start, y_start, length = match
-
-    score = 0
-
-    for i in range(length):
-
-        score += cost(s[y_start + i], t[x_start + i])
-
-    return score
-
-
-def fasta(s, t, ktup):
-
-    # s is the database string. s is the y-axis
-    # t is the query string. t is the x-axis
-    # ktup: seed size.
-
-    lookup = {"".join(i): [] for i in product(alphabet, repeat=ktup)}
-
-    for i in range(len(s) - ktup + 1):
-
-        word = s[i:i + ktup]
-        lookup[word].append(i)
-
-    matches = []
-
-    for i in range(len(t) - ktup + 1):
-
-        word = t[i:i + ktup]
-
-        for j in lookup[word]:
-
-            matches.append((i, j, ktup))
-
-    def extend(match):
-
-        # Extend along the diagonal as long as the score improves (and never below some score value)
-
-        x_start, y_start, length = match
-        x_end, y_end = x_start + length, y_start + length
-
-        while x_start > 0 and y_start > 0:
-
-            if cost(t[x_start], s[y_start]) > 0:
-
-                x_start -= 1
-                y_start -= 1
-
-            else:
-
-                break
-
-        while x_end < len(t) and y_end < len(s):
-
-            if cost(t[x_end], s[y_end]) > 0:
-
-                x_end += 1
-                y_end += 1
-
-            else:
-
-                break
-
-        return x_start, y_start, x_end - x_start
-
-    # print(lookup)
-    # print(matches)
-    # print([score_match(s, t, match) for match in matches])
-
-    extended = [extend(match) for match in matches]
-
-    banded(s, t, extended)
-
-    # Identifying potential diagonals: high-scoring gap-less diagonals contain several 'seeds' of perfect matches.
-    # Since the alginment is gapless, all perfect match regions reside on the same diagonal.
-
-    # Let ktup be a parameter denoting the seed length of interest.
-    # Assume that s is the database and t is the query string, e.g. |s| >> |t|
-
-    # Prepare an index table which, for any sequence x of length ktup, contains the list of x's positions in S.
-
-
-    pass
-
 
 
 fasta("ABCCCABABACABCABCABCBAABABCCCAAACBCBCBABCABCBABBBCABCA", "AAACCBACBAC", 3)
